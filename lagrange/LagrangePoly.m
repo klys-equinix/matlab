e8a0c72e99a8138d8ba6946b3f1a94c22c91bfc4
losiu97 <konrad.lys1997@gmail.com>
2018-03-10 09:51:37 +0100

    Correction

--- lagrange/LagrangePoly.m
+++ lagrange/LagrangePoly.m
@@ -1,12 +1,12 @@
 function [coeffs] = LagrangePoly(X, index)
     n = length(X);
-    coeffs = zeros(n);
+    coeffs = zeros(1, n);
     div_val = 1;
     first_iter = 1;
     for i=1:n
         if i~=index
             div_val = div_val * (X(index) - X(i));
-            poly = [1; -X(i)];
+            poly = [1 -X(i)];
             if first_iter == 1
                 coeffs = poly;
                 first_iter = 0;
